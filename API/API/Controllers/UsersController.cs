06a0476a1a61648c58ec8f8d225489a679b87221
Vina <vina.nalawade@gmail.com>
2022-01-07 14:10:29 +0000

    Section 2- Building API

---
+++ API/API/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+       private DataContext _context;
+        public UsersController(DataContext context )
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<AppUser>> Get()
+        {
+          return  await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<AppUser> Get(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+
+
+    }
+}
